c5ac880c3d496f6a3a2dee781b0cfc35b722a32e
Syed Abdullah Abbas Sohail <abdullahsohail7@hotmail.com>
2022-07-19 01:18:59 +0500

    weather

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 
 import "./App.css";
-// import UilReact from "@iconscout/react-unicons/icons/uil-react";
 import TopButtons from "./components/TopButtons";
 import Inputs from "./components/Inputs";
-// import TimeAndLocation from "./components/TimeandLocation";
 import TimeandLocation from "./components/TimeandLocation";
 import TemperatureAndDetails from "./components/TemperatureAndDetails";
 import Forecast from "./components/Forecast";
-// import getWeatherData from "./services/weatherService";
 import getFormattedWeatherData from "./services/weatherService";
 import { useEffect, useState } from "react";
 function App() {
